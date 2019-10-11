7157c822ca3eabac5654cfe54ec966df13ccca54
Antonio Cavedoni <antonio@cavedoni.org>
2019-10-11 17:00:56 +0200

    wait_until_done=True

--- example_error.py
+++ example_error.py
@@ -2,8 +2,5 @@ from urlreader import URLReader
 from urlreader.utils import callback
 
 
-reader = URLReader(timeout=1)
+reader = URLReader(timeout=1, wait_until_done=True)
 reader.fetch("https://www.doesnot-exist.forsure/", callback)
-
-while not reader.done:
-    reader.continue_runloop()
