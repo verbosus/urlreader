26ac1ae0e0fcca45c7de9b01ac75dfc1c1975c06
Antonio Cavedoni <antonio@cavedoni.org>
2019-10-11 17:01:30 +0200

    wait_until_done=True

--- example_timeout.py
+++ example_timeout.py
@@ -2,8 +2,5 @@ from urlreader import URLReader
 from urlreader.utils import callback
 
 
-reader = URLReader(timeout=0.001)
+reader = URLReader(timeout=0.001, wait_until_done=True)
 reader.fetch("https://www.amazon.com/", callback)
-
-while not reader.done:
-    reader.continue_runloop()
