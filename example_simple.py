77b73290d0cab7480d10778706c057b84e7c5330
Antonio Cavedoni <antonio@cavedoni.org>
2019-10-11 16:59:47 +0200

    wait_until_done=True

--- example_simple.py
+++ example_simple.py
@@ -2,8 +2,5 @@ from urlreader import URLReader
 from urlreader.utils import callback
 
 
-reader = URLReader()
+reader = URLReader(wait_until_done=True)
 reader.fetch("https://www.apple.com/", callback)
-
-while not reader.done:
-    reader.continue_runloop()
